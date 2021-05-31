8e0743f02a49950987be5f703f3c11eec91f8030
Jeremy <jeremyeparker@yahoo.com>
2021-05-30 18:37:51 -0700

    idk what i changed here tbh

--- src/main/java/com/atsignJar/mongodb/MongodbApplication.java
+++ src/main/java/com/atsignJar/mongodb/MongodbApplication.java
@@ -2,8 +2,11 @@ package com.atsignJar.mongodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import resource.BookController;
 
 @SpringBootApplication
+
 public class MongodbApplication {
 
 	public static void main(String[] args) {
