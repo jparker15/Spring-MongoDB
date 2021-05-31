2ad21fc7468c316fb48467fc6cafd70f09bda103
Jeremy <jeremyeparker@yahoo.com>
2021-05-30 18:36:28 -0700

    repo interface extends mongo repo

--- src/main/java/repository/BookRepo.java
+++ src/main/java/repository/BookRepo.java
@@ -1,2 +1,8 @@
-package repository;public interface BookRepo {
+package repository;
+
+import model.Book;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface BookRepo extends MongoRepository<Book,Integer> {
+
 }
